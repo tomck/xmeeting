c919162f3e8afff1cbabb08d81a1909303872e44
hfriederich <hfriederich@users.sourceforge.net>
2009-01-14 09:21:13 +0000

    Remove unused files
    git-svn-id: svn://svn.code.sf.net/p/xmeeting/code/XMeeting/trunk@16 c1127893-963e-0410-b184-b9f2f23da79e

--- Source/Framework/XMOpalPatches.cpp
+++
@@ -1,8 +0,0 @@
-/*
- * $Id: XMOpalPatches.cpp,v 1.4 2006/08/05 20:23:26 hfriederich Exp $
- *
- * Copyright (c) 2006 XMeeting Project ("http://xmeeting.sf.net").
- * All rights reserved.
- * Copyright (c) 2006 Hannes Friederich. All rights reserved.
- */
-
