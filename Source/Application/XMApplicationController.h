a80ae92657e7a78a0b36f3781611bfbe780bf8a6
hfriederich <hfriederich@users.sourceforge.net>
2009-01-14 08:03:26 +0000

    Test SVN header revision
    git-svn-id: svn://svn.code.sf.net/p/xmeeting/code/XMeeting/trunk@11 c1127893-963e-0410-b184-b9f2f23da79e

--- Source/Application/XMApplicationController.h
+++ Source/Application/XMApplicationController.h
@@ -1,9 +1,11 @@
 /*
- * $Id: XMApplicationController.h,v 1.25 2008/10/24 12:22:02 hfriederich Exp $
- *
- * Copyright (c) 2005-2008 XMeeting Project ("http://xmeeting.sf.net").
+ * Copyright (c) 2005-2009 XMeeting Project ("http://xmeeting.sf.net").
  * All rights reserved.
- * Copyright (c) 2005-2008 Hannes Friederich. All rights reserved.
+ * Copyright (c) 2005-2009 Hannes Friederich. All rights reserved.
+ *
+ * $Revision$
+ * $Author$
+ * $Date$
  */
 
 #ifndef __XM_APPLICATION_CONTROLLER_H__
