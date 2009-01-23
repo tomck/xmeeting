de29fde549a456fb2ad1e8d965a57669fb8e20a7
hfriederich <hfriederich@users.sourceforge.net>
2009-01-23 10:32:52 +0000

    Rework Address book plugin support & scripting
    git-svn-id: svn://svn.code.sf.net/p/xmeeting/code/XMeeting/trunk@39 c1127893-963e-0410-b184-b9f2f23da79e

---
+++ AddressBookPlugin/XMAddressBookPlugin.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2007-2009 XMeeting Project ("http://xmeeting.sf.net").
+ * All rights reserved.
+ * Copyright (c) 2007-2009 Hannes Friederich. All rights reserved.
+ *
+ * $Revision$
+ * $Author$
+ * $Date$
+ */
+
+#ifndef __XM_ADDRESS_BOOK_PLUGIN_H__
+#define __XM_ADDRESS_BOOK_PLUGIN_H__
+
+#import <AddressBook/AddressBook.h>
+
+@interface XMAddressBookPlugin : NSObject {
+
+}
+
+- (void)performActionForPerson:(ABPerson *)person identifier:(NSString *)identifier;
+- (BOOL)shouldEnableActionForPerson:(ABPerson *)person identifier:(NSString *)identifier;
+- (NSString *)titleForPerson:(ABPerson *)person identifier:(NSString *)identifier;
+- (NSString *)actionProperty;
+
+@end
+
+#endif // __XM_ADDRESS_BOOK_PLUGIN_H__
